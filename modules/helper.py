41646c8807370042b2c757f3a1f663942c398ceb
Kunal  Singhal <knsn1994@gmail.com>
2014-07-15 13:03:44 -0700

    added rule match function

--- modules/helper.py
+++ modules/helper.py
@@ -18,18 +18,27 @@ class Word:
     def __init__(self, isTer, text):
         self.isTerminal = isTer
         self.text = text
-    def match(self, lis):
+    def match(self, word):
         if self.isTerminal:
-            return lis[0] == self.text
+            return self.text.lower() == word['string'].lower()
         else:
-            for x in lis:
-                if x == self.text:
+            for x in word.values():
+                if x.lower() == self.text.lower():
                     return True
             return False
 
 class Rule:
     def __init__(self, lis):
         self.words = [Word(x[0], x[1]) for x in lis]
+    def match(self, tweet):
+        i, j = 0, 0
+        while i < len(self.words) and j < len(tweet.features):
+            if self.words[i].match(tweet.features[j]):
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i == len(self.words)
 
 
 def tag(s):
